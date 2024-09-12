6be1f8c89ec76b7ccdff354f87c6a290ebcf6037
max <max@gmail.com>
2024-09-12 10:33:57 +0900

    basic of selector in recoil

--- react-todo-app/src/index.tsx
+++ react-todo-app/src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { darkTheme } from "./theme";
 import reportWebVitals from './reportWebVitals';
-import { RecoilRoot } from 'recoil';
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
+
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -74,6 +76,7 @@ a {
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
@@ -82,7 +85,7 @@ root.render(
         <App />
       </ThemeProvider>
     </RecoilRoot>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 reportWebVitals();
